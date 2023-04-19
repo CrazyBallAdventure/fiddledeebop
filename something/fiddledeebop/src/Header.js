fc455a087a3cec265d711247a9503fa64ce5d074
CrazyBallAdventure <123973434+CrazyBallAdventure@users.noreply.github.com>
2023-04-18 22:50:04 -0400

    talked about how cool that chameleon was

---
+++ something/fiddledeebop/src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+class Header extends React.Component{
+render(){
+    return(
+        <h1>I’m over here taking a nap rn I got a pillow under my head rn, sleeping so soundly, I’m sleepy as frick man, I’m a snoozer man</h1>
+    )
+}
+}
+
+export default Header;
